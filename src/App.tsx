771fb1ef8fd4cafd3e4d9a875b7beed53faa3d86
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-09-24 00:43:35 -0300

    style: configuração dos estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+
+import GlobalStyle from "./styles/global";
 
 const App: React.FC = () => (
-  <h1>Hello World</h1>
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
