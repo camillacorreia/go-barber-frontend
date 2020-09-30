bf3e4d5b269d31ade5a1fb9cf8072001337513de
Camilla Correia <camillajesuscorreia@hotmail.com>
2020-09-29 21:36:50 -0300

    feat: estrutura de toasts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import SignIn from "./pages/SignIn";
 import GlobalStyle from "./styles/global";
 
+import ToastContainer from "./components/ToastContainer";
 import { AuthProvider } from "./hooks/AuthContext";
 
 const App: React.FC = () => (
@@ -10,7 +11,7 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
-
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
